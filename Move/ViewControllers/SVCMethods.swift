c0d9b24471b4dd47f3f55678fa08543787677e5f
Jake Gray <grayjake14@gmail.com>
2018-04-24 15:00:01 -0600

    organize SVC

---
+++ Move/ViewControllers/SVCMethods.swift
@@ -0,0 +1,9 @@
+//
+//  SVCMethods.swift
+//  Move
+//
+//  Created by Jake Gray on 4/24/18.
+//  Copyright © 2018 Jake Gray. All rights reserved.
+//
+
+import Foundation
---
+++ Move/ViewControllers/SVCSearchBar.swift
@@ -0,0 +1,9 @@
+//
+//  SVCSearchBar.swift
+//  Move
+//
+//  Created by Jake Gray on 4/24/18.
+//  Copyright © 2018 Jake Gray. All rights reserved.
+//
+
+import Foundation
